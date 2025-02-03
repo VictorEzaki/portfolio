fe341a1f8796468c3d595fcd1fd581a8292d58bd
Victor Ezaki <victorezaki19@gmail.com>
2025-02-03 16:50:40 -0300

    fix: Arrumando conteúdos da formação na primeira Page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
                     <div id="content">
                         <h1 id="studant">Estudante de Engenharia de Software</h1>
                         <h2 id="fullname">Victor Takeyoshi Kieper Ezaki</h2>
-                        <p id="resume">Olá, mundo! Me chamo Victor, tenho 21 anos e sou natural de Joinville. Atualmente, curso o 1º período de Engenharia de Software no Centro Universitário Católica de Santa Catarina – Joinville. Tenho grande interesse em criar soluções FullStack, porém, minha afinidade e foco estão voltados ao desenvolvimento BackEnd.</p>
+                        <p id="resume">Olá, mundo! Me chamo Victor, tenho 21 anos e sou natural de Joinville. Atualmente, curso o 1º período de Análise e Desenvolvimento de Sistemas na faculdade Senac Joinville. Tenho grande interesse em criar soluções FullStack, porém, minha afinidade e foco estão voltados ao desenvolvimento BackEnd.</p>
                         <Link to='/projetos'>Projetos</Link>
                     </div>
                 </div>
